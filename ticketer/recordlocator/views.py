63bac617744a65d0e62e4e4e5eafa34e4e4936fd
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-05-11 21:13:27 +0000

    Some cleanup

--- ticketer/recordlocator/views.py
+++ ticketer/recordlocator/views.py
@@ -6,9 +6,20 @@ from django.http import HttpResponse
 from recordlocator import generator
 
 def record_locators(request):
+    """ View that generates at least one record locator, generating 'n' if
+    specified through a query parameter. """
 
-    record_locator = generator.safe_generate()
-    response = {'record_locators': [record_locator]}
-    data = json.dumps(response)
+    num_locators = int(request.GET.get('n', 1))
 
-    return HttpResponse(data, content_type='application/json')
+    record_locators = generate_locators(num_locators)
+    data = {'record_locators': record_locators}
+    response = json.dumps(data)
+ 
+    return HttpResponse(response, content_type='application/json')
+
+
+def generate_locators(n=1):
+    """ Generate 'n' unique record locators. """
+
+    record_locators = [generator.safe_generate() for r in range(0, n)]
+    return record_locators
