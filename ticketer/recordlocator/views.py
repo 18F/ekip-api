6da998ffb52cd3bf5fd2c0fff29699abdaf29daa
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-05-15 17:20:58 +0000

    Add a limit on the number of record locators that can be requested at once

--- ticketer/recordlocator/views.py
+++ ticketer/recordlocator/views.py
@@ -1,9 +1,13 @@
 import json
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 
 from recordlocator import generator
+
+
+# The maximum number of record locators that can be requested
+MAX_REQUESTABLE = 50
 
 def record_locators(request):
     """ View that generates at least one record locator, generating 'n' if
@@ -11,6 +15,10 @@ def record_locators(request):
 
     num_locators = int(request.GET.get('n', 1))
 
+    if num_locators > MAX_REQUESTABLE:
+        return HttpResponseBadRequest(
+            'Maximum of %s record locators can be requested' % MAX_REQUESTABLE)
+        
     record_locators = generate_locators(num_locators)
     data = {'record_locators': record_locators}
     response = json.dumps(data)
