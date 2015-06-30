ed40b666fea0b103406f89adeb08b9cd822807bb
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-06-30 13:00:19 +0000

    More tests around site_type determination, and slugs

--- ekip/nationalparks/tests.py
+++ ekip/nationalparks/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase 
 
-from .management.commands.passes import phone_number, name_to_site_type
+from .management.commands.passes import phone_number, determine_site_type
 from .management.commands.slugify import nps_slug, nf_slug, nwr_slug
+from .management.commands.slugify import blm_slug, nra_slug, other_slug
 
 class DataCleanupTests(TestCase):
 
@@ -26,28 +27,48 @@ class DataCleanupTests(TestCase):
         phone, extension = phone_number('580-555-3165; 580-622-3161')
         self.assertEqual(phone, '580-555-3165')
 
-    def test_name_to_site_type(self):
+    def test_determine_site_type(self):
         self.assertEqual(
             'NF',
-            name_to_site_type('Angeles NF - LA River District'))
+            determine_site_type('Angeles NF - LA River District', ''))
         self.assertEqual(
             'NRA',
-            name_to_site_type('Golden Gate NRA - Alcatraz Visitors Center'))
+            determine_site_type(
+                'Golden Gate NRA - Alcatraz Visitors Center', ''))
         self.assertEqual(
             'NWR',
-            name_to_site_type('Tule Lake NWR'))
+            determine_site_type('Tule Lake NWR', ''))
         self.assertEqual(
             'NWR',
-            name_to_site_type('Kilauea Point National Wildlife Refuge'))
+            determine_site_type('Kilauea Point National Wildlife Refuge', ''))
         self.assertEqual(
             'NHS',
-            name_to_site_type('Abraham Lincoln Birthplace NHS'))
+            determine_site_type('Abraham Lincoln Birthplace NHS', ''))
         self.assertEqual(
             'NPS',
-            name_to_site_type('Assateague Island National Seashore'))
+            determine_site_type(
+                'Assateague Island National Seashore', 'http://www.nps.gov/assateague'))
+        self.assertEqual(
+            'BLM',
+            determine_site_type(
+                'La Cienagas National Conservation Area',
+                'http://www.blm.gov/lacienaga.html'))
+        self.assertEqual(
+            'NF', 
+            determine_site_type(
+                'Chugach National Forest', 'http://www.fs.fed.us/chugach'))
+        self.assertEqual(
+            'NWR',
+            determine_site_type(
+                'Neosho National Fish Hatchery', 'http://www.fws.gov/neosho/'))
+        self.assertEqual(
+            'NF',
+            determine_site_type(
+                'Glacier Public Service Center', 'http://www.fs.usda.gov/mbs'))
 
 
 class SlugifyTests(TestCase):
+
     def test_nps_slug(self):
         url = 'http://www.nps.gov/isro/'
         slug = nps_slug(url)
@@ -71,3 +92,25 @@ class SlugifyTests(TestCase):
 
         name = "Nisqually NWR"
         self.assertEqual('nwr-nisqually', nwr_slug(name))
+
+        name = "Neosho National Fish Hatchery"
+        self.assertEqual('nwr-neosho-national-fish-hatchery', nwr_slug(name))
+
+    def test_blm_slug(self):
+        name = 'Worland BLM Field Office'
+        self.assertEqual('blm-worland', blm_slug(name))
+
+        name = 'Las Cruces BLM District Office'
+        self.assertEqual('blm-las-cruces', blm_slug(name))
+
+    def test_nra_slug(self):
+        name = "Hells Canyon NRA - Clarkston Field Office"
+        self.assertEqual('nra-hells-canyon-clarkston', nra_slug(name))
+
+
+    def test_other_slug(self):
+        name = "The King Center"
+        self.assertEqual('oth-the-king-center', other_slug(name))
+
+        name = "Explore Arizona Information Center"
+        self.assertEqual('oth-explore-arizona', other_slug(name))
