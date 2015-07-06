82b248aba790e6a6dd9a4020f1286b7d26565e66
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-07-06 17:44:36 +0000

    Use direct to template routing to make things simpler

--- ekip/everykid/urls.py
+++ ekip/everykid/urls.py
@@ -1,15 +1,18 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from .views import (
-    main_landing, get_your_pass, plan_your_trip, learn, student_pass,
-    educator_passes)
+    plan_your_trip, learn, student_pass, educator_passes)
 
 urlpatterns = patterns(
     '',
-    url(r'^$', main_landing, name="main_landing"),
+    url(r'^$', TemplateView.as_view(
+        template_name="main_landing.html"), name="main_landing"),
     url(r'get-your-pass/fourth-grader', student_pass, name="student_pass"),
     url(r'get-your-pass/educator', educator_passes, name="educator_passes"),
-    url(r'get-your-pass/', get_your_pass, name="get_your_pass"),
-    url(r'plan-your-trip/', plan_your_trip, name="plan_your_trip"),
+    url(r'get-your-pass/', TemplateView.as_view(
+        template_name="get_your_pass.html"), name="get_your_pass"),
+    url(r'plan-your-trip/', TemplateView.as_view(
+        template_name="plan_your_trip.html"), name="plan_your_trip"),
     url(r'learn/', learn, name="learn"),
 )
--- ekip/everykid/views.py
+++ ekip/everykid/views.py
@@ -1,19 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 
-def main_landing(request):
-    return render(
-        request, 
-        'main_landing.html',
-        {}
-    )
-
-def get_your_pass(request):
-    return render(
-        request, 
-        'get_your_pass.html',
-        {}
-    )
-
 def plan_your_trip(request):
     return render(
         request, 
