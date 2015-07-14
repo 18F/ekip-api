8597324ac0a531142c58d2fb6b0fc7ce7c0c629d
xtine <christine@fieryorange.com>
2015-07-13 17:29:04 -0700

    student voucher page (with random barcode id)

---
+++ ekip/everykid/static/js/voucher.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  var recordLocator = $('.voucher-barcode'),
+      defaultID = $(recordLocator).find('.voucher-barcode-id').text(),
+      randomNumber = Math.round(Math.random() * 9999999 - 1000000) + 1000000,
+      randomID = randomNumber.toString();
+
+  $(recordLocator).find('.voucher-barcode-id').remove();
+  $(recordLocator).find("img").JsBarcode(randomID,{format:"CODE128",displayValue:true,fontSize:20, height:40});
+
+});
