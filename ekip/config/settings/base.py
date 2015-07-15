878f260186ddea23a86b5830a3bad797b35191d8
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-07-15 19:01:30 +0000

    Add django storages, boto so that we can start hosting static files in S3

--- ekip/config/settings/base.py
+++ ekip/config/settings/base.py
@@ -45,6 +45,7 @@ THIRD_PARTY_APPS = (
     'corsheaders',
     'localflavor',
     'formtools',
+    'storages',
 )
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- ekip/config/settings/production.py
+++ ekip/config/settings/production.py
@@ -1,4 +1,5 @@
 import dj_database_url
+import os
 
 from .base import *
 
@@ -9,6 +10,19 @@ ALLOWED_HOSTS = ["*"]
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config()
+
+
+AWS_STORAGE_BUCKET_NAME = os.getenv('EKIP_STATIC_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = 's3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_ACCESS_KEY_ID = os.getenv('EKIP_AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('EKIP_AWS_SECRET_ACCESS_KEY')
+
+
+# Don't add complex authentication related query parameters for requests
+AWS_QUERYSTRING_AUTH = False
 
 LOGGING = {
     'version': 1,
