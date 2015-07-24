9721a755b73db0d7a380e232571d067b34434f70
xtine <christine@fieryorange.com>
2015-07-24 01:37:06 -0700

    student flow

--- ekip/everykid/static/js/voucher.js
+++ ekip/everykid/static/js/voucher.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
 
   var recordLocator = $('.voucher-barcode'),
-      defaultID = $(recordLocator).find('.voucher-barcode-id').text(),
-      randomNumber = Math.round(Math.random() * 9999999 - 1000000) + 1000000,
-      randomID = randomNumber.toString();
+      locatorID = $(recordLocator).find('.voucher-barcode-id').text();
 
   $(recordLocator).find('.voucher-barcode-id').remove();
-  $(recordLocator).find("img").JsBarcode(randomID,{format:"CODE128",displayValue:true,fontSize:20, height:40});
+  $(recordLocator).find("img").JsBarcode(locatorID,{format:"CODE128",displayValue:true,fontSize:20, height:40});
 
-  $(".voucher-barcode-image").JsBarcode(randomID,{format:"CODE128",displayValue:true,fontSize:20, height:40});
-
+  $(".voucher-barcode-image").JsBarcode(locatorID,{format:"CODE128",displayValue:true,fontSize:20, height:40});
 
 });
