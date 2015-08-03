0d283b78752bd88f15a36c02c1d0733cc90c2fb3
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-08-03 16:28:40 +0000

    Ensure tests run without login required middelware

--- ekip/config/settings/test.py
+++ ekip/config/settings/test.py
@@ -1,5 +1,17 @@
 from .base import *
 
+#Do not include the LoginRequiredMiddleware
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
 
 # The test database can just be sqlite.
 DATABASES = {
