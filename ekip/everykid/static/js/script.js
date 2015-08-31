a00787fb5bc20363f3b4943d199fd6ec3e318252
xtine <christine@fieryorange.com>
2015-08-31 16:01:23 +0100

    about page - click to expand/collapse text

--- ekip/everykid/static/js/script.js
+++ ekip/everykid/static/js/script.js
@@ -12,26 +12,48 @@ $(function () {
     $(this).addClass('active');
 
     $('.animate-line').fadeIn(1200);
+
   });
 
+  // toggle agency descriptions
   $('#about .participating li').click(function() {
 
     $(this).find('.description').fadeToggle();
+
+    $(this).toggleClass('open').toggleClass('closed');
+
+    $('.closed .expand').html('click to expand');
+    $('.open .expand').html('click to collapse');
+
   });
 
+  $('#about .participating li h4').hover(function() {
+
+    $(this).next().fadeIn();
+
+  }, function () {
+
+    $(this).next().fadeOut();
+
+  })
+
   // scroll down to pass/field trip sites
-  // if larger than tablet
   if($('#plan-your-trip .sites').length) {
 
+    // don't scroll if tablet size or less
     if($(window).width() > 1024) {
       $('html, body').animate({
         scrollTop: $(".sites-container").offset().top
      }, 1200);
     }
+
   }
 
+  // educator form (back to edit)
   $('#educators .back').click(function() {
+
     history.go(-1);
+
   });
 
 }());
