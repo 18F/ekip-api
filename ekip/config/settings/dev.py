10515657b56123335fbecfd15f249ccf5a0884e3
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-08-31 14:40:57 +0000

    merge

--- ekip/config/settings/dev.py
+++ ekip/config/settings/dev.py
@@ -9,9 +9,9 @@ DATABASES = {
         'NAME': 'ekip',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'ekip',
-        'PASSWORD': 'ekip',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'PASSWORD': 'pike',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
