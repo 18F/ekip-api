8245e147a992f56eab3485669a5e8c11e38301cf
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-08-31 21:15:12 +0000

    Make educator flow work

--- ekip/everykid/views.py
+++ ekip/everykid/views.py
@@ -137,12 +137,12 @@ class EducatorFormPreview(FormPreview):
     form_template = 'get-your-pass/educator_passes.html'
     preview_template = 'get-your-pass/educator_passes_preview.html'
 
-    @staticmethod
     def done(self, request, cleaned_data):
         educator = Educator(
             name=cleaned_data['name'],
             work_email=cleaned_data['work_email'],
             organization_name=cleaned_data['organization_name'],
+            org_or_school=cleaned_data['org_or_school'],
             address_line_1=cleaned_data['address_line_1'],
             address_line_2=cleaned_data['address_line_2'],
             city=cleaned_data['city'],
