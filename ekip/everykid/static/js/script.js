617cb2c78b0e64f75bbda4a969b3503f15add91d
xtine <christine@fieryorange.com>
2015-09-15 15:27:10 -0700

    educator front-end form validation

--- ekip/everykid/static/js/script.js
+++ ekip/everykid/static/js/script.js
@@ -1,6 +1,12 @@
 $(function () {
   'use strict';
 
+  function emailValidation(email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+  }
+
+
   $('.lets-go, .educator-begin, .lets-get-going, .about-more, .parents-more').click(function(e) {
 
     e.preventDefault();
@@ -50,6 +56,87 @@ $(function () {
     }
 
   }
+
+
+  // educator form error checking
+  $('#educators button[type="submit"]').click(function(e) {
+
+    e.preventDefault();
+
+    var formErrors = false;
+
+    // check text fields
+    $('#educators input[type="text"]').each(function() {
+
+      if($(this).val() == '' && $(this).attr('id') !== 'formtools_address_line_2') {
+
+        $(this).parent().addClass('error');
+
+        formErrors = true;
+      }
+      else {
+        $(this).parent().removeClass('error');
+      }
+    });
+
+
+    // validate email
+    if(!emailValidation($('#formtools_work_email').val())) {
+
+      $('.work_email').addClass('error');
+
+      formErors = true;
+    }
+    else {
+      $('.work_email').removeClass('error');
+    }
+
+
+    // check radio inputs
+    if(!$('#formtools_org_or_school_0').is(':checked') && !$('#formtools_org_or_school_1').is(':checked')) {
+
+      $('.org_or_school').addClass('error');
+
+      formErrors = true;
+    }
+    else {
+      $('.org_or_school').removeClass('error');
+    }
+
+
+    // check dropdown
+    if(!$("select option:selected").val()) {
+      $('.state').addClass('error');
+
+      formErrors = true;
+    }
+    else {
+      $('.state').removeClass('error');
+    }
+
+
+    // check number input
+    if(!$('#educators input[type="number"]').val()) {
+      $('.num_students').addClass('error');
+
+      formErrors = true;
+    }
+    else {
+      $('.num_students').removeClass('error');
+    }
+
+    if(formErrors) {
+
+      $('.errors').remove();
+
+      $('form').prepend('<div class="errors">Please correctly fill out the highlighted fields below:</div>');
+    }
+    else {
+      $('#educators form').submit();
+    }
+
+  });
+
 
   // educator form (back to edit)
   $('#educators .back').click(function() {
