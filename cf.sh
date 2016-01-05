671584fc25cbed7fb5f3ee5601abd0ad1e87b5dd
Trevor Cole <trevor_cole@nps.gov>
2016-01-05 09:59:30 -0500

    Multiple changes #1
    Multitude of changes:
    
    1) cf.sh change to work around licensing issue with NewRelic was
    recommended by Shashank.
    
    2) Most everything else in this round of changes pertains to the master
    list of changes from Julie, outlined below
    
    a) Textual changes throughout the site
    b) Modal window spawned on external link clicks on the planning page

--- cf.sh
+++ cf.sh
@@ -1,3 +1,3 @@
 echo '-----Starting APP ----------'
 cd ekip
-newrelic-admin run-program waitress-serve --port=$VCAP_APP_PORT ekip.config.wsgi:application
+waitress-serve --port=$VCAP_APP_PORT ekip.config.wsgi:application
