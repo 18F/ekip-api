226cbea2d1d37e0825cfc992ae430f02cd6ce8e2
Trevor Cole <trevor_cole@nps.gov>
2016-01-06 17:59:22 -0500

    Django template settings change
    Added Templates block to the settings file for dev to avoid getting
    deprecated error messages.

--- ekip/config/settings/dev.py
+++ ekip/config/settings/dev.py
@@ -2,13 +2,35 @@ from .base import *
 
 DEBUG = True
 
+#TEMPLATE_DEBUG = DEBUG
+
 TEMPLATES = [
     {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
         'OPTIONS': {
-            'debug': DEBUG,
-        },
-    },
+        	'debug': DEBUG,
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages', 
+                'django.core.context_processors.request']
+        }
+    }
 ]
+
+#TEMPLATES = [
+#    {
+#        'OPTIONS': {
+#            
+#        },
+#    },
+#]
 
 DATABASES = {
     'default': {
