d04fa1046571cf81a107b3d5720b979418c91eb7
Trevor Cole <trevor_cole@nps.gov>
2016-08-31 20:57:36 -0400

    Try/Except Added to Field_Trip

--- ekip/nationalparks/management/commands/field_trip.py
+++ ekip/nationalparks/management/commands/field_trip.py
@@ -6,9 +6,12 @@ from nationalparks.management.commands import ficorcleaner as fc
 from nationalparks.models import FieldTripSite, BestVisitTime, YouthFacility
 
 #Begin by cleaning house!
-FieldTripSite.objects.all().delete()
-BestVisitTime.objects.all().delete()
-YouthFacility.objects.all().delete()
+try:
+    FieldTripSite.objects.all().delete()
+    BestVisitTime.objects.all().delete()
+    YouthFacility.objects.all().delete()
+except:
+    pass
     
 NAME_TO_ABBR = {r[1]: r[0] for r in FieldTripSite.AGENCY_CHOICES}
 NAME_TO_ABBR['Army Corps'] = 'USACE'
