d01cfef98f22c0bb4c0276cafd931526abbecf1a
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2016-11-21 21:21:56 +0000

    Fix api issues

--- ekip/redemption/urls.py
+++ ekip/redemption/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from redemption.views import redeem_confirm, redeem_for_site, sites_for_state
 from redemption.views import get_passes_state, statistics, tables, csv_redemption
 
-urlpatterns = patterns(
-    '', 
+urlpatterns = [ 
     url(r'statistics/$', statistics),
     url('data/exchanges/$', csv_redemption, name='exchanges_data'),
     url(r'data/$', tables),
@@ -11,4 +10,4 @@ urlpatterns = patterns(
     url(r'done/(?P<slug>[-\w]+)/$', redeem_confirm),
     url(r'sites/', sites_for_state),
     url(r'^$', get_passes_state),
-)
+]
--- ekip/ticketer/recordlocator/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(regex=r'^$', view=views.record_locators, name='generate_ticket'),
-]
