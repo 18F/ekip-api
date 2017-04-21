11050d4500dde5fb6fe511b54a78b8432a640a67
James C. Scott <jamesc.scott@gsa.gov>
2017-04-21 09:09:20 -0400

    Fix typo in comment

--- ekip/everykid/views.py
+++ ekip/everykid/views.py
@@ -20,7 +20,7 @@ STATES['PR'] = 'Puerto Rico'
 STATES['VI'] = 'Virgin Islands'
 
 def planyourtrip(request):
-    """ We launched with /planyoutrip printed on the vouchers.  Make that URL
+    """ We launched with /planyourtrip printed on the vouchers.  Make that URL
     work. """
 
     return HttpResponseRedirect(reverse('plan_your_trip'))
